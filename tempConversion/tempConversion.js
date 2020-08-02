3e1e19f962b967be901227b97b039a217776aca3
Sanjana <sanjhemdev@gmail.com>
2020-08-03 05:04:37 +0530

    Complete tempConversion exercise

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = fTemp => {
+  let c = (fTemp - 32) * (5/9);
+  let final = Math.round((c + Number.EPSILON) * 10) / 10;
+  return final;
 }
 
-const ctof = function() {
-
+const ctof = cTemp => {
+  let f = cTemp * (9/5) + 32;
+  let final = Math.round((f + Number.EPSILON) * 10) / 10;
+  return final;
 }
 
 module.exports = {
