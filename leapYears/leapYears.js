dd224773c679e9f0f4773b7ba1c5e5c4b70e7aac
Sanjana <sanjhemdev@gmail.com>
2020-08-03 04:52:23 +0530

    Complete leapYears exercise

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,8 @@
-const leapYears = function() {
-
+const leapYears = year => {
+    return (year % 4 !== 0) ? false
+    : (year % 100 === 0 && year % 400 === 0) ? true
+    : (year % 100 !== 0 && year % 4 === 0) ? true
+    : false;
 }
 
 module.exports = leapYears
