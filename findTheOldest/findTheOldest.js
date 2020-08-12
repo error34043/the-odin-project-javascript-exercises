2d74021fbe0ccb2975804f7f2eaa232bf84a9dfd
Sanjana <sanjhemdev@gmail.com>
2020-08-13 03:26:39 +0530

    Compleete findTheOldest exercise

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-let findTheOldest = function() {
+const findTheOldest = peopleList => {
+    const today = new Date();
+    const currentYear = today.getFullYear();
+    
+    let oldest;
+    let ages = [];
+    for (let i = 0; i < peopleList.length; i++) {
+        if (peopleList[i].yearOfDeath) {
+            ages.push(peopleList[i].yearOfDeath - peopleList[i].yearOfBirth);
+        } else {
+            ages.push(currentYear - peopleList[i].yearOfBirth);
+        };
+    };
 
-}
+    const maxAgeIndex = ages.indexOf(Math.max(...ages));
+
+    return peopleList[maxAgeIndex];
+};
 
 module.exports = findTheOldest
+
