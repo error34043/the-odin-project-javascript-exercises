89c950d3a7aeb5fce9accbaca28fab05f8609d57
Sanjana <sanjhemdev@gmail.com>
2020-08-13 03:06:47 +0530

    Complete fibonacci exercise

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
-}
+const fibonacci = inp => {
+    const num = Number(inp);
+    if (num < 0) {
+        return 'OOPS';
+    };
+    const fibArr = [1, 1];
+    for (let i = 0; i < num - 2; i++) {
+        fibArr.push(fibArr[i] + fibArr[i+1]);
+    };
+    return fibArr[fibArr.length - 1];
+};
 
 module.exports = fibonacci
