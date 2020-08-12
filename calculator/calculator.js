196e18e12c1d094273db712c6f5a8a8a5df7b2ee
Sanjana <sanjhemdev@gmail.com>
2020-08-12 18:48:50 +0530

    Complete calculator exercise

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,26 +1,34 @@
-function add () {
-	
-}
+const add = (a, b) => a + b;
 
-function subtract () {
-	
-}
+const subtract = (a, b) => a - b;
 
-function sum () {
-	
-}
+function sum (arr) {
+	let sum = 0;
+	const length = arr.length;
+	for (let i = 0; i < length; i++) {
+		sum += arr[i];
+	};
+	return sum;
+};
 
-function multiply () {
-	
-}
+function multiply (arr) {
+	let product = 1;
+	const length = arr.length;
+	for (let i = 0; i < length; i++) {
+		product *= arr[i];
+	};
+	return product;
+};
 
-function power() {
-	
-}
+const power = (a, b) => Math.pow(a,b);
 
-function factorial() {
-	
-}
+function factorial(a) {
+	let result = 1;
+	for (let i = a; i > 0; i--) {
+		result *= i;
+	};
+	return result;
+};
 
 module.exports = {
 	add,
