732900cd8b7b3b9d02a2caf97bb8195434838174
Sanjana <sanjhemdev@gmail.com>
2020-08-13 03:16:18 +0530

    Complete getTheTitles exercise

--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
-}
+const getTheTitles = bookList => {
+    let titles = [];
+    for (let i = 0; i < bookList.length; i++) {
+        titles.push(bookList[i].title);
+    };
+    return titles;
+};
 
 module.exports = getTheTitles;
