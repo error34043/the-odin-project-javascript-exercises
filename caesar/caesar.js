7ac0f2b818125e9379dacade4f0bfbf23f8aa742
Sanjana <sanjhemdev@gmail.com>
2020-08-13 23:11:12 +0530

    Complete caesar exercise

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,5 +1,53 @@
-const caesar = function() {
+const caesar = (strInp, shiftInp) => {
+    let outputArr = [];
+    let letterRegex = /[a-z]/i;
+    let shift = 0;
 
-}
+    if (shiftInp > 26 || shiftInp < -26) {
+        shift = shiftInp % 26;
+    } else {
+        shift = shiftInp;
+    };
+
+    for (let i = 0; i < strInp.length; i++) {
+        if (letterRegex.test(strInp[i]) === true) {
+            let charcode = 0;
+            charcode = strInp.charCodeAt(i);
+
+            if (charcode >= 65 && charcode <= 90) {
+                charcode += shift;
+                if (charcode < 65) {
+                    remainder = 65 - charcode;
+                    charcode = 91 - remainder;
+                    outputArr.push(String.fromCharCode(charcode));
+                } else if (charcode > 90) {
+                    remainder = charcode - 90;
+                    charcode = 64 + remainder;
+                    outputArr.push(String.fromCharCode(charcode));
+                } else {
+                    outputArr.push(String.fromCharCode(charcode));
+                }
+            } else {
+                charcode += shift;
+                if (charcode < 97) {
+                    remainder = 97 - charcode;
+                    charcode = 122 - remainder;
+                    outputArr.push(String.fromCharCode(charcode));
+                } else if (charcode > 122) {
+                    remainder = charcode - 122;
+                    charcode = 96 + remainder;
+                    outputArr.push(String.fromCharCode(charcode));
+                } else {
+                    outputArr.push(String.fromCharCode(charcode));
+                };
+            }
+        } else {
+            outputArr.push(strInp[i]);
+        };
+    };
+
+    output = outputArr.join("");
+    return output;
+};
 
 module.exports = caesar
